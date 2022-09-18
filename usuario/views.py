b02da06214cad2cab94ccfb3a52b5e0772148933
Rokukawa <gustavo.camargo@sensedata.com.br>
2022-09-18 14:37:26 -0300

    criando as rotas e inicializando as funções do crud de usuario

--- usuario/views.py
+++ usuario/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from usuario.models import Usuario
 
 # Create your views here.
+ 
+def login(request):
+    pass
+    
+
+def logout(request):
+    pass
+
+
+def cadastro_usuario(request):
+    pass
+
+def dashboard(request):
+    pass
