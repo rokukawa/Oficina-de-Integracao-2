1b79c53a67a70cb94e5e2cd8bcb2854ad20a5b17
Rokukawa <gustavo.camargo@sensedata.com.br>
2022-09-18 14:34:53 -0300

    adicionando o modelo de criação de usuario no banco de dados

--- usuario/models.py
+++ usuario/models.py
@@ -1,3 +1,14 @@
+from ast import fix_missing_locations
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class Usuario(AbstractUser):
+    username = models.CharField(max_length=30, unique=False)
+    email = models.EmailField(max_length=255, unique=True)
+    telefone_celular = models.CharField(max_length=20, null=True, blank=True)
+    telefone_residencial = models.CharField(max_length=20, null=True, blank=True)
+    USERNAME_FIELD = 'email' 
+    REQUIRED_FIELDS = []
